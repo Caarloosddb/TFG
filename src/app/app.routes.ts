16d620e552278f37659dedbca62b6bc6923e4306
Martin Cabal <mcabaldeltoro@gmail.com>
2025-04-03 14:35:27 +0200

    pag futbol y nav #1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { FutbolComponent } from './futbol/futbol.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'home', component: HomeComponent},
+    {path: 'futbol', component: FutbolComponent},
+
 
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: '**', redirectTo: 'home'},
---
+++ src/app/futbol/futbol.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-futbol',
+  imports: [RouterLink],
+  templateUrl: './futbol.component.html',
+  styleUrl: './futbol.component.scss'
+})
+export class FutbolComponent {
+
+}
