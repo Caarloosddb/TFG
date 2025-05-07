218ebd2a32ddf98751e7772080d932c2901620f3
AlbertoAldana <alberto.ald.ort@gmail.com>
2025-05-07 18:41:46 +0200

    correcion slider y separadores secciones noticias

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { NewsService, Article } from '../../services/news.service';
@@ -17,10 +17,8 @@ export class HomeComponent implements OnInit {
   nbaNews: Article[]     = [];
   f1News: Article[]      = [];
   hideAds: boolean = false;
-  images = ['anuncio1.jpg', 'anuncio2.jpg', 'anuncio3.jpg'];  // Lista de imágenes
+  images = ['anuncio1.jpg', 'anuncio2.jpg', 'anuncio3.jpg'];
   image = 0;
-
-
 
   constructor(private newsService: NewsService, private cdRef: ChangeDetectorRef) {}
 
@@ -30,14 +28,16 @@ export class HomeComponent implements OnInit {
     this.newsService.getF1News()    .subscribe(a => this.f1News     = a);
     this.anuncio();
   }
-  anuncio() {
+
+  anuncio(): void {
     setInterval(() => {
       this.image = (this.image + 1) % this.images.length;
+      this.cdRef.detectChanges();  // Para asegurarse de que la vista se actualiza
     }, 5000);
   }
-  
+
   hideAd(): void {
-    console.log("Anuncio oculto");
     this.hideAds = true;
+    console.log("Anuncio oculto");
   }
 }
