de8884dc0d448ce49773f3cbcb79ff5e13ee2f2f
Martin Cabal <mcabaldeltoro@gmail.com>
2025-05-12 13:08:07 +0200

    pag noticias

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { NewsService, Article } from '../../services/news.service';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { FooterComponent } from '../../footer/footer.component';
@@ -8,36 +7,45 @@ import { FooterComponent } from '../../footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ CommonModule, RouterLink, NavbarComponent, FooterComponent ],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  futbolNews: Article[]  = [];
-  nbaNews: Article[]     = [];
-  f1News: Article[]      = [];
-  hideAds: boolean = false;
+  futbolNews: Article[] = [];
+  nbaNews: Article[]    = [];
+  f1News: Article[]     = [];
+
+  hideAds = false;
   images = ['anuncio1.jpg', 'anuncio2.jpg', 'anuncio3.jpg'];
   image = 0;
+  showAll: boolean = false;
 
-  constructor(private newsService: NewsService, private cdRef: ChangeDetectorRef) {}
+  constructor(
+    private newsService: NewsService,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.newsService.getFutbolNews().subscribe(a => this.futbolNews = a);
     this.newsService.getNbaNews()   .subscribe(a => this.nbaNews    = a);
     this.newsService.getF1News()    .subscribe(a => this.f1News     = a);
-    this.anuncio();
+    this.startSlider();
   }
 
-  anuncio(): void {
+  private startSlider(): void {
     setInterval(() => {
       this.image = (this.image + 1) % this.images.length;
-      this.cdRef.detectChanges();  // Para asegurarse de que la vista se actualiza
+      this.cdRef.detectChanges();
     }, 5000);
   }
 
   hideAd(): void {
     this.hideAds = true;
-    console.log("Anuncio oculto");
+    console.log('Anuncio oculto');
   }
 }
